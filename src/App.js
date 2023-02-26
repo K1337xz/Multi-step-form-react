68e0a4c1edcd8c71110cd3d76555db0a8c693052
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-02-26 19:17:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Selectplan from "./pages/Selectplan";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-		<Route>
+		<Route basename="/multi-step-form-react">
 			<Route path="/multi-step-form-react" element={<Mainpage />} />
 			<Route
 				path="/multi-step-form-react/select"
