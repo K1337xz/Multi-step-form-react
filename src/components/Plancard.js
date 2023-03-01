d671ef5c39da0ed937d6326804ef474e56e45c46
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-03-01 21:49:00 +0100

    add select plan,and styles

--- src/components/Plancard.js
+++ src/components/Plancard.js
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function Plancard(props) {
-	return <div className="card"></div>;
+	function toggleClick(e) {
+		const allElements = document.querySelectorAll(".card");
+		for (let i = 0; i < allElements.length; i++) {
+			if (allElements[i].classList.contains("active")) {
+				allElements[i].classList.remove("active");
+			}
+			e.currentTarget.classList.add("active");
+		}
+	}
+	return (
+		<div
+			className="card"
+			onClick={(e) => {
+				toggleClick(e);
+			}}
+			id={props.id}
+		>
+			<div className="cardImage">
+				<img src={props.src} className="image" alt={props.altText} />
+			</div>
+			<div className="cardContent">
+				<h2>{props.cardName}</h2>
+				<p>{props.cardPrice}</p>
+			</div>
+		</div>
+	);
 }
--- src/pages/Selectplan.js
+++ src/pages/Selectplan.js
@@ -1,6 +1,9 @@
 import React from "react";
 import SideBar from "../components/SideBar";
-
+import Plancard from "../components/Plancard";
+import arcade from "../assets/images/icon-arcade.svg";
+import advanced from "../assets/images/icon-advanced.svg";
+import pro from "../assets/images/icon-pro.svg";
 export default function Selectplan() {
 	return (
 		<>
@@ -14,7 +17,33 @@ export default function Selectplan() {
 			<div className="rightContent">
 				<h1>Select your plan</h1>
 				<p>You have option of monthly or yearly billing</p>
-				<div className="selectPlanForm"></div>
+				<div className="selectPlanForm">
+					<Plancard
+						src={arcade}
+						cardName="Arcade"
+						cardPrice="$9/mo"
+						id="arcade"
+					/>
+					<Plancard
+						src={advanced}
+						cardName="Advanced"
+						cardPrice="$12/mo"
+						id="advanced"
+					/>
+					<Plancard
+						src={pro}
+						cardName="Pro"
+						cardPrice="$15/mo"
+						id="pro"
+					/>
+				</div>
+				<div className="lowerCheckbox">
+					<label htmlFor="lowerCheck">
+						Monthly
+						<input type="checkbox" />
+						<span>Yearly</span>
+					</label>
+				</div>
 			</div>
 		</>
 	);
